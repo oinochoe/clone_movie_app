3640c197ed7ea5dfa0fee5434e318f8f9b426762
oinochoe <oinochoe@naver.com>
2020-06-11 09:53:55 +0900

    import export 예쩨

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
-    return <div className='App'>안녕</div>;
+    return (
+        <div className='App'>
+            안녕 <Potato />
+        </div>
+    );
 }
 
 export default App;
