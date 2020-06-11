93c31a1f3c70e9f23a47bc420b508c5d0a44d934
oinochoe <oinochoe@naver.com>
2020-06-11 09:55:49 +0900

    내부에서 바로 컴포넌트 만들어서 쓰기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Potato from './Potato';
 
+function Dragon() {
+    return <h4>Dragon</h4>;
+}
+
 function App() {
     return (
         <div className='App'>
             안녕 <Potato />
+            <Dragon />
         </div>
     );
 }
