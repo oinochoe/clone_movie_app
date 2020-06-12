5c3576c384b605ec134e37c2815937393716b491
oinochoe <oinochoe@naver.com>
2020-06-12 10:05:10 +0900

    map 함수로 데이터 바인딩하기 props로..

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import React from 'react';
 import Potato from './Potato';
 
 // 구조 분해 할당으로 props 사용하면 . 연산자를 사용하지 않아도 됩니다.
-function Dragon({ fav }) {
-    return <h4>I like {fav}</h4>;
+function Food({ name, picture }) {
+    return (
+        <div>
+            <h4>I like {name}</h4>
+            <img src={picture} alt='' />
+        </div>
+    );
 }
+
+const foodLike = [
+    {
+        name: 'Kimchi',
+        image: 'http://aeriskitchen.com/wp-content/uploads/2020/06/ChapSsalTteok2_01-.jpg',
+    },
+    {
+        name: 'sunpong',
+        image: 'http://aeriskitchen.com/wp-content/uploads/2020/05/Cucumber_and_Crab_Stick_Salad_01-.jpg',
+    },
+    {
+        name: 'bibimp',
+        image: 'http://aeriskitchen.com/wp-content/uploads/2020/05/Spicy_Tiny_Anchovy_Side_01-.jpg',
+    },
+];
 
 function App() {
     return (
         <div className='App'>
             안녕 <Potato />
-            <Dragon fav='kimchi' something={true} papapapa={['hello', 1, 2, 3, 4, true]} />
+            {foodLike.map((dish) => (
+                <Food name={dish.name} picture={dish.image} />
+            ))}
         </div>
     );
 }
