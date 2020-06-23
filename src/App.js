f72a2781454ab7d650ad9ccdcc02b190ea139ee2
yeongminkim <oinochoe@naver.com>
2020-06-24 00:08:49 +0900

    영화 리스트 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
     state = {
@@ -11,7 +12,7 @@ class App extends React.Component {
             data: {
                 data: { movies },
             },
-        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
         // console.log(movies);
         this.setState({ movies, isLoading: false });
     };
@@ -19,8 +20,17 @@ class App extends React.Component {
         this.getMovies();
     }
     render() {
-        const { isLoading } = this.state; // destructuring
-        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+        const { isLoading, movies } = this.state; // destructuring
+        return (
+            <div>
+                {isLoading
+                    ? 'Loading...'
+                    : movies.map((movie) => {
+                          console.log(movie);
+                          return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />;
+                      })}
+            </div>
+        );
     }
 }
 
