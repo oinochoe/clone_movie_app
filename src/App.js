6e6866a704c2c0dba51cac9411bdc5589b53fba7
yeongminkim <oinochoe@naver.com>
2020-06-23 23:35:04 +0900

    axios으로 영화데이터 받아와서 console에 알맹이만 찍기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    getMovies = async () => {
+        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies.data.data.movies);
+    };
+    componentDidMount() {
+        this.getMovies();
+    }
     render() {
-        return <div />;
+        const { isLoading } = this.state; // destructuring
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
     }
 }
 
