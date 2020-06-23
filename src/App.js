5f46ca2ede7847ad21decfd0f930c33b360e2ebc
yeongminkim <oinochoe@naver.com>
2020-06-23 23:42:22 +0900

    loading 값 상태 바꾸기

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
             },
         } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
         // console.log(movies);
-        this.setState({ movies });
+        this.setState({ movies, isLoading: false });
     };
     componentDidMount() {
         this.getMovies();
