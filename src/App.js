ca2566a6cfd663d7bfe8b1ff883a1b5a48edcf62
yeongminkim <oinochoe@naver.com>
2020-06-23 23:37:47 +0900

    구조분해할당으로 아름다운 코드

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ class App extends React.Component {
         movies: [],
     };
     getMovies = async () => {
-        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-        console.log(movies.data.data.movies);
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies);
     };
     componentDidMount() {
         this.getMovies();
