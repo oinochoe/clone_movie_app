32218a6497a65e0d30d9f194e0206b49793e648e
yeongminkim <oinochoe@naver.com>
2020-06-26 21:49:44 +0900

    movie app styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
     state = {
@@ -38,6 +39,7 @@ class App extends React.Component {
                                     title={movie.title}
                                     summary={movie.summary}
                                     poster={movie.medium_cover_image}
+                                    genres={movie.genres}
                                 />
                             );
                         })}
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
         <div className='movie'>
             <img src={poster} alt={title} title={title} />
             <div className='movie__data'>
                 <h3 className='movie__title'>{title}</h3>
                 <h5 className='movie__year'>{year}</h5>
+                <ul className='movie__genres'>
+                    {genres.map((genre, index) => {
+                        return (
+                            <li key={index} className='movie__genre'>
+                                {genre}
+                            </li>
+                        );
+                    })}
+                </ul>
                 <p className='movie__summary'>{summary}</p>
             </div>
         </div>
@@ -19,6 +29,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
