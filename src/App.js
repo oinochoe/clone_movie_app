ebe68ddecbd0130555e6e60560c02bf9dff39c8a
yeongminkim <oinochoe@naver.com>
2020-06-27 16:17:57 +0900

    navigation 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
     return (
         <HashRouter>
-            <Route />
+            <Navigation />
+            <Route path='/' exact={true} component={Home} />
+            <Route path='/about' component={About} />
         </HashRouter>
     );
 }
---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+    return (
+        <div className='about__container'>
+            <span>"Freedom is the freedom to say"</span>
+            <span>-George Orwell, 1984</span>
+        </div>
+    );
+}
