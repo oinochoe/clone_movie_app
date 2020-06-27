8c6efded5510372181916003140f87bde1aa5cad
yeongminkim <oinochoe@naver.com>
2020-06-27 16:45:01 +0900

    보기페이지 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import About from './routes/About';
 import Home from './routes/Home';
-import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Navigation />
             <Route path='/' exact={true} component={Home} />
             <Route path='/about' component={About} />
+            <Route path='/movie-detail' component={Detail} />
         </HashRouter>
     );
 }
