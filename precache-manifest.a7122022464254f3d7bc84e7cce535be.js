39d484265599f9f7d53f35f657c698f3436f2fcc
yeongminkim <oinochoe@naver.com>
2020-06-27 16:56:06 +0900

    Updates

---
+++ precache-manifest.a7122022464254f3d7bc84e7cce535be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285f5e4fb6a3496fdf8bb30f77c6ecbd",
+    "url": "/clone_movie_app/index.html"
+  },
+  {
+    "revision": "971d5a736c6cb954b19d",
+    "url": "/clone_movie_app/static/css/main.b46fc656.chunk.css"
+  },
+  {
+    "revision": "f0cccc372f728152e5be",
+    "url": "/clone_movie_app/static/js/2.f958f727.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone_movie_app/static/js/2.f958f727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971d5a736c6cb954b19d",
+    "url": "/clone_movie_app/static/js/main.8703cfd4.chunk.js"
+  },
+  {
+    "revision": "2e921756a7896780a590",
+    "url": "/clone_movie_app/static/js/runtime-main.b8f20688.js"
+  }
+]);
